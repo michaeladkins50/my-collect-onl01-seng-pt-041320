2ae9c12668982049587749b15353e16861583895
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-20 15:14:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
 
+def my_collect(array)
+  collection = []
+  i = 0
+if array == languages
+  while i < array.length
+      languages.each do|language| collection << language.upcase
 
+        end
+        i += 1
+      end
+  end
+if array == students
+  while i < students.length
+    students.each do|student| collection << student.split(" ").first
+
+
+    end
+    i += 1
+  end
+end
+end
